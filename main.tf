d8f33575dbb2c5cfb0d01425ff7c30a1431e1cf0
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 08:18:14 -0600

    yes

--- main.tf
+++ main.tf
@@ -106,11 +106,11 @@ resource "aws_lb" "my_lb" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.elb_sg.id]
  subnets = [for subnet in aws_subnet.my_subnet : subnet.id]
-}
 
-  tags = {
+tags = {
     Name = "my-load-balancer"
   }
+}
 
 
 resource "aws_lb_target_group" "my_target_group" {
