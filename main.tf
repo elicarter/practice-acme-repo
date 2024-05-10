a3973e0d4b0be02867e71a536aa2201ac46e11e6
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 08:12:41 -0600

    fix

--- main.tf
+++ main.tf
@@ -58,7 +58,10 @@ resource "google_compute_instance" "vm_instance" {
 
 resource "aws_subnet" "my_subnet" {
   vpc_id     = var.existing_vpc_id
-  cidr_block = var.subnet_cidr
+  cidr_block = cidrsubnet(var.subnet_cidr, 4, count.index)
+  count = length(var.availability_zones)
+
+  availability_zone = var.availability_zones[count.index]
 }
 
 resource "aws_security_group" "elb_sg" {
