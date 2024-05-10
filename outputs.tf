66f722e5ca592cef139a83040c91a8aa15a6f92f
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 07:18:54 -0600

    updating configs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "instance_ip" {
-  value = aws_instance.AcmeDemoServer.public_ip
+  value = aws_instance.AcmeDemoServer[count.index].public_ip
 }
 
 output "ip" {
-  value = google_compute_instance.vm_instance.network_interface.0.network_ip
+  value = google_compute_instance.vm_instance[count.index].network_interface.0.network_ip
 }
 
 output "public_dns" {
-  value = aws_instance.ec2_instance.*.public_dns
+  value = aws_instance.AcmeDemoServer.*.public_dns
 }
