9c99057961da235d2798d49f7bb4780fdf3af1cc
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 07:22:32 -0600

    fixings

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "instance_ip" {
-  value = aws_instance.AcmeDemoServer[count.index].public_ip
+output "instance_public_ips" {
+  value = [for instance in aws_instance.AcmeDemoServer : instance.public_ip]
 }
 
 output "ip" {
-  value = google_compute_instance.vm_instance[count.index].network_interface.0.network_ip
+  value = [for instance in google_compute_instance.vm_instance : instance.network_interface.0.network_ip]
 }
 
 output "public_dns" {
